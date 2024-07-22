6a4a65343cfba2addc6d1ffe83d5f59aec3dfd66
Wang Chengwei <jaune162@126.com>
2024-07-22 16:53:19 +0800

    配置修改

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   //apiUrl: 'http://192.168.1.21:8087',
   apiUrl: 'http://localhost:8087',
-  isAllowAppMenu: false,
+  isAllowAppMenu: true,
   rootDepartmentName: '深蓝社区',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.darkblue.dblug.org',
+  apiUrl: 'http://124.222.32.147:8087/',
   isAllowAppMenu: true,
   rootDepartmentName: '深蓝社区',
 };
